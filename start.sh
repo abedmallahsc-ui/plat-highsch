06ed12af10ce21cefd4bd5fa6c65bb7ed9ed4db4
Abedelghanimallah <abedmallahsc@gmail.com>
2026-08-01 15:36:10 +0300

    Fix Render start script

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-cd /Users/apple/Desktop/sch.p
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 exec gunicorn --bind 0.0.0.0:$PORT schoolplatform.wsgi:application
