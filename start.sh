198295da5ce76badc3f1adaa95f9d70cd03198cc
Abedelghanimallah <abedmallahsc@gmail.com>
2026-08-01 14:50:45 +0300

    Fix Render startup script

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 cd /Users/apple/Desktop/sch.p
-./.venv/bin/python manage.py migrate --noinput
-./.venv/bin/python manage.py collectstatic --noinput
-exec ./.venv/bin/gunicorn --bind 0.0.0.0:${PORT:-8000} schoolplatform.wsgi:application
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+exec gunicorn --bind 0.0.0.0:$PORT schoolplatform.wsgi:application
